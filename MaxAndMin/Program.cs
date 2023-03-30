2a39c0cad0a5657c302cbee9d1a479fb3fe7ef6d
besser <besser9009@mail.ru>
2023-03-30 18:52:07 +0300

    add code search max and min

---
+++ MaxAndMin/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+string num1 = Console.ReadLine();
+int number1 = Convert.ToInt32(num1);
+Console.Write("Введите первое число: ");
+string num2 = Console.ReadLine();
+int number2 = Convert.ToInt32(num2);
+if (number1 > number2){
+    Console.Write("min = ");
+    Console.WriteLine(number2);
+    Console.Write("max = ");
+    Console.WriteLine(number1);
+}
+else{
+    Console.Write("min = ");
+    Console.WriteLine(number1);
+    Console.Write("max = ");
+    Console.WriteLine(number2);
+}
