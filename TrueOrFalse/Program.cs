1adc20a64e7777b6caf07755281b0b7463cebec7
besser <besser9009@mail.ru>
2023-03-30 19:10:48 +0300

    add code search even or odd

---
+++ TrueOrFalse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+double number = Convert.ToInt32(num);
+if (number % 2 == 0){
+    Console.WriteLine("Yes!");
+}
+else {
+    Console.WriteLine("No...");
+}
