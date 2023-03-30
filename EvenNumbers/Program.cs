1801f2b16a240178fa5b415859bd70362b967268
besser <besser9009@mail.ru>
2023-03-30 19:56:49 +0300

    add code search all even numbers

---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите максимальное колличество чисел: ");
+string MaxNum = Console.ReadLine();
+int N = Convert.ToInt32(MaxNum);
+int number = 1;
+while (number < N + 1){
+    if (number % 2 == 0){
+        Console.WriteLine(number);
+    }
+    number += 1;
+}
